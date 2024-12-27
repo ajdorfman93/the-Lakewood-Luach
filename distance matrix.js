4f1ab85c92393ef3c94cb1fdcf0e978522c063e3
ajdorfman93 <dorfmanart@gmail.com>
2024-12-26 21:45:18 -0500

    Initial commit

---
+++ distance matrix.js
@@ -0,0 +1,3 @@
+'https://maps.googleapis.com/maps/api/distancematrix/json?destinations=New%20York%20City%2C%20NY&origins=Washington%2C%20DC&units=imperial&key=AIzaSyBOtVjKr3D0vZmwg1QlxCy6SR4rVQenaPU'
+
+'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyBOtVjKr3D0vZmwg1QlxCy6SR4rVQenaPU'
